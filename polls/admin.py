d8ba60aadc3d64b7c1b7f87841d70522dc59a4ae
dogurasu <dugs134@gmail.com>
2020-12-23 15:15:07 -0800

    init commit

--- polls/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# import models here
-from .models import Question, Choice
-
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
